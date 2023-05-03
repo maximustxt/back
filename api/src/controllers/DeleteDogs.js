6189f09081fa55c04dcc08bd080dc48cdbbe922b
uliseschambi <uliseschambi@gmail.com>
2023-05-03 14:35:59 -0300

    delete ok

--- api/src/controllers/DeleteDogs.js
+++ api/src/controllers/DeleteDogs.js
@@ -1,13 +1,13 @@
-// const { Dog } = require("../db");
+const { Dog } = require('../db');
 
-// const DeleteDogs = async (id) => {
-//   const DogEliminar = Dog.findOne({ where: { id } });
-//   if (!DogEliminar) {
-//     throw Error("No existe ese perro.");
-//   } else {
-//     await Dog.destroy({ where: { id } });
-//     return "Dog eliminada con exito";
-//   }
-// };
+const DeleteDogs = async id => {
+  const DogEliminar = Dog.findOne({ where: { id } });
+  if (!DogEliminar) {
+    throw Error('No existe ese perro.');
+  } else {
+    await Dog.destroy({ where: { id } });
+    return 'Dog eliminada con exito';
+  }
+};
 
-// module.exports = DeleteDogs;
+module.exports = DeleteDogs;
